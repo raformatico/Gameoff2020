2e6262952c7cf48cd73c975f4e9b7dd308b0b5db
raformatico <raformatico@raformatico.com>
2020-11-14 17:28:49 +0100

    Added Tween for the audio

--- R-Audio/audio-manager.gd
+++ R-Audio/audio-manager.gd
@@ -4,6 +4,7 @@ extends Node
 var song_i: = 0 
 
 onready var songs: = $songs.get_children()
+onready var tween : Tween = $Tween
 
 func _ready() -> void:
 	start_all()
@@ -18,7 +19,25 @@ func start_all() -> void:
 		songs[i].play()
 
 
+func next_song_tween() -> void:
+	var first_audio : AudioStreamPlayer = songs[song_i]
+	var second_audio : AudioStreamPlayer = songs[(song_i + 1) % songs.size()]
+	tween.interpolate_property(first_audio, "volume_db", first_audio.volume_db, -80, 1,Tween.TRANS_SINE,Tween.EASE_IN)
+	tween.interpolate_property(second_audio, "volume_db", second_audio.volume_db, 0, 1,Tween.TRANS_SINE,Tween.EASE_OUT)
+	tween.start()
+	song_i = (song_i + 1) % songs.size()
+
+
 func next_song() -> void:
 	songs[song_i].volume_db = -80
 	song_i = (song_i + 1) % songs.size()
 	songs[song_i].volume_db = 0
+
+
+
+
+
+
+
+
+
