a7b3b75b75526f57bf8b4f5be3d538d9d20da996
raformatico <raformatico@raformatico.com>
2020-11-19 17:07:25 +0100

    Adjustments to audio fades

--- R-Audio/audio-manager.gd
+++ R-Audio/audio-manager.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("next_song"):
+		#next_song_tween()
 		next_song()
 
 func start_all() -> void:
@@ -22,8 +23,8 @@ func start_all() -> void:
 func next_song_tween() -> void:
 	var first_audio : AudioStreamPlayer = songs[song_i]
 	var second_audio : AudioStreamPlayer = songs[(song_i + 1) % songs.size()]
-	tween.interpolate_property(first_audio, "volume_db", first_audio.volume_db, -80, 1,Tween.TRANS_SINE,Tween.EASE_IN)
-	tween.interpolate_property(second_audio, "volume_db", second_audio.volume_db, 0, 1,Tween.TRANS_SINE,Tween.EASE_OUT)
+	tween.interpolate_property(first_audio, "volume_db", first_audio.volume_db, -80, 2,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+	tween.interpolate_property(second_audio, "volume_db", -30, 0, 0.5,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 	tween.start()
 	song_i = (song_i + 1) % songs.size()
 
