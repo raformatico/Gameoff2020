cc9dcd445fdc9784c138ef41e0f88f796a83a61b
matildagame <55888362+matildagame@users.noreply.github.com>
2020-11-27 19:14:18 +0100

    "take" function added to clickable. It may not work...

---
+++ GameEngine/Clickable.gd
@@ -0,0 +1,22 @@
+extends StaticBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func take():
+	var mesh=get_parent()
+	
+	if mesh!=null:
+		mesh.visible=false
