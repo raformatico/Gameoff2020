8c376e450084afd08ab3906d272b25956a529916
matildagame <55888362+matildagame@users.noreply.github.com>
2020-11-28 20:32:27 +0100

    DJ2 trial

---
+++ J-dj/DJTest.gd
@@ -0,0 +1,79 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var channel =[]
+var discs=[]
+
+var start_time=0
+export var slot_time=250
+
+export var segment_duration=2
+
+var n_discs_playing=0
+
+var main_plato=null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	channel.append($StaticBody/AudioStreamPlayer2D)
+	channel.append($StaticBody2/AudioStreamPlayer2D)
+	channel.append($StaticBody3/AudioStreamPlayer2D)
+
+
+	var discs_ = get_tree().get_nodes_in_group("Disc")
+#	discs.append($StaticBody)
+#	discs.append($StaticBody2)
+#	discs.append($StaticBody3)
+	
+
+	for disc in discs_:
+		if main_plato==null or disc.main_rythm:
+			main_plato=disc
+			
+		discs.append(disc)
+		disc.set_clock_manager(self)
+		disc.connect("start",self, "_on_disc_start")
+		disc.connect("stop",self, "_on_disc_stop")
+
+#	channel[0].play()
+#	channel[1].play(2)
+#	channel[2].play(4)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var play_time=""
+	
+	for disc in discs:
+		play_time+=str(disc.get_playback_position())+"\n"
+	$Label.text=play_time
+
+func get_next_instant():
+	var time=OS.get_system_time_msecs()-start_time
+	return (time%slot_time)/1000.0
+	
+func _on_disc_start(plato):
+	if n_discs_playing==0: # first disc sets the tempo
+		start_time=OS.get_system_time_msecs()
+		
+	n_discs_playing+=1
+	
+	check_sync()
+	
+func _on_disc_stop(plato):
+	n_discs_playing-=1
+
+func check_sync():
+	
+	for disc in discs:
+		if disc.on:	
+			if !disc.main_rythm:
+				var diff=abs(disc.get_playback_position()-main_plato.get_playback_position())/disc.get_total_length()
+				disc.set_sync(diff)
+				print(diff)
+			else:			
+				disc.set_sync(0)
+				print("Main")
--- J-dj/Disc.gd
+++ J-dj/Disc.gd
@@ -3,25 +3,64 @@ extends StaticBody
 
 # Declare member variables here. Examples:
 # var a = 2
+signal start()
+signal stop()
 # var b = "text"
 
+export var main_rythm=false
 export var speed=1
 export var on=false
+var clock_manager=null
+
+var audio_length=0
+
+var led=null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	audio_length=$AudioStreamPlayer2D.stream.get_length()
+	led=get_node("../Led")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if on:
+		var time=$AudioStreamPlayer2D.get_playback_position()*1.0/audio_length
+		
 		rotate(Vector3(0,1,0),delta*speed)
-
+		# $Label.text=str($AudioStreamPlayer2D.get_playback_position ( ))
+		# $CSGBox2.material.albedo_color=Color(time,1,0)
 
 func _on_StaticBody_input_event(camera, event, click_position, click_normal, shape_idx):
 	if (event is InputEventMouseButton and event.is_pressed() == true):
+		var time=0
 		on=!on	
 		if on:
-			$AudioStreamPlayer2D.play()
+			emit_signal("start",self)
+			if clock_manager!=null:
+				time=clock_manager.get_next_instant()
+			$AudioStreamPlayer2D.play(time)
+			#led.set_color(Color.red)
 		else:
+			emit_signal("stop",self)
 			$AudioStreamPlayer2D.stop()
+			led.set_color(Color.gray)
+
+func set_clock_manager(manager_):
+	clock_manager=manager_
+
+func get_playback_position():
+	return $AudioStreamPlayer2D.get_playback_position()
+
+func get_total_length():
+	return $AudioStreamPlayer2D.stream.get_length()
+
+func set_sync(syncr):
+	if syncr>0.75:
+		led.set_color(Color.red)	
+	elif syncr>0.5:
+		led.set_color(Color.orange)	
+	elif syncr>0.25:
+		led.set_color(Color.yellow)
+	else:
+		led.set_color(Color.green)		
+	
