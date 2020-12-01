26d0041b6cf77f371baa43488c9bc0381daed176
matildagame <55888362+matildagame@users.noreply.github.com>
2020-12-01 23:07:27 +0100

    Fixed door return

--- Scenes/Intro/intro.gd
+++ Scenes/Intro/intro.gd
@@ -4,3 +4,7 @@ var room := "res://Scenes/museum-final/Room.tscn"
 
 func _on_VideoPlayer_finished() -> void:
 	get_tree().change_scene(room)
+
+func _process(delta):
+	if Input.is_action_pressed("stop"):
+		get_tree().change_scene(room)
